100753c7b631705113cb92a094c5455a2a8a680b
juanesmalagon2911-star <juanesmalagon2911@gmail.com>
2026-09-21 08:12:26 -0500

    Update inventario.js

--- inventario.js
+++ inventario.js
@@ -1,14 +1,15 @@
 const Modulo = require('./modulo');
-
+ 
 class Inventario extends Modulo {
-
+ 
     constructor() {
-        super('Inventario');
+        super('Inventario', 'Inventario'); 
     }
-
+ 
     mostrar(item) {
         return `ID: ${item.id} | Producto: ${item.producto} | Categoría: ${item.categoria} | Stock: ${item.stock}`;
     }
 }
-
+ 
 module.exports = Inventario;
+ 
